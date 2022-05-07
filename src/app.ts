408255676034dc17337d45f8a0f70d69df2424ba
Th-release.cth <84012697+th-release@users.noreply.github.com>
2022-05-07 19:06:26 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -14,17 +14,19 @@ app.get("/",(req: Server[0], res: Server[1], next: Server[2]) => {
 app.post("/login", async(req: Server[0], res: Server[1], next: Server[2]) => {
     const { username, password } = req.query
     const [user] = await db.select('*').from('Accounts').where('username', String(username))
-    if (user && sha3_256(user.salt + password) === user.password) return res.send({ 
-        success: true, 
-        token: jwt.sign({ username }, SECRETHASH, { expiresIn: '2h' }), 
-        status: 200, msg: "로그인 되었습니다."
-    }).status(200)
-    else res.send({ 
-        success: false, 
-        msg: "비밀번호가 틀렸거나 유저가 없습니다."
-    }); 
+    if (user && sha3_256(user.salt + password) === user.password) { 
+        return res.send({ 
+            success: true, 
+            token: jwt.sign({ username }, SECRETHASH, { expiresIn: '2h' }), 
+            status: 200, msg: "로그인 되었습니다."
+        }).status(200)
+    } else {
+        res.send({ 
+            success: false, 
+            msg: "비밀번호가 틀렸거나 유저가 없습니다."
+        });
+    } 
     return res.status(404)
-  }
-);
+});
 
 export default app;
